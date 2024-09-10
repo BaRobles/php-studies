db2e7ce87f6a9ac32432912951344fba025eec42
BaRobles <119386284+BaRobles@users.noreply.github.com>
2024-09-10 10:14:07 -0300

    learned about callbacks

--- public/index.php
+++ public/index.php
@@ -24,17 +24,56 @@
 
 // CLOSURES (FUNÇÕES ANÔNIMAS)
 
-function teste($name)
-{
-  // closure
-  $person = function() use ($name){
-    return $name;
-  };
-  return $person;
-
-}
+// function teste($name)
+// {
+//   // closure
+//   $person = function() use ($name){
+//     return $name;
+//   };
+//   return $person;
+// }
 
 // chamamos 2x () por causa da closure
 // O primeiro () chama a função teste.
 // O segundo () chama a função anônima (closure) que foi retornada.
-var_dump(teste('Alexandre')());
+// var_dump(teste('Alexandre')()); // string(9) "Alexandre"
+
+// CALLBACKS
+
+function teste($name){
+  return 'Olá, meu nome é ' . $name . '. ';
+};
+
+function teste2($callback)
+{
+  // podemos colocar um if para ter certeza de que o parâmetro passado é uma função:
+  if(is_callable($callback)){
+    return $callback('Barbara');
+  }
+};
+
+// note que ao passar uma função como parâmetro para outra, precisamos passá-la como STRING!
+echo teste2('teste'); // Olá, meu nome é Barbara.
+
+function funcNome($name)
+{
+  return 'Essa é a ' . $name . '. ';
+}
+// podemos fazer callback assim também:
+echo call_user_func('funcNome', 'Bárbara'); //Essa é a Bárbara.
+// O primeiro argumento é a função e os demais os seus parâmetros.
+
+class User
+{
+  public function userTeste($name, $age)
+  {
+    return 'Oiee, meu nome é ' . $name . '!' . ' E eu tenho ' . $age . ' anos. '; 
+  }
+}
+
+// só instanciamos a classe, pois ela não é estática (public static...). Caso fosse, poderíamos chamar call_user_func direto.
+$user = new User;
+
+// passando um objeto com parâmetros para call_user_func
+echo call_user_func([$user, 'userTeste'], 'Joaquim', 98); // Oiee, meu nome é Joaquim!
+
